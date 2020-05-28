55bdeafd5f02b02230ac75fbfedccda9ed396ee4
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-28 12:33:28 -0700

    Added User, Movie, and Rating class

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    # flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.app = flask_app
@@ -18,6 +18,57 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 
     print('Connected to the db!')
 
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement = True,
+                        primary_key = True,
+                        )
+    email = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer, 
+                         autoincrement = True, 
+                         primary_key = True,
+                         )
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+class Rating(db.Model):
+    """A movie rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, 
+                          autoincrement = True, 
+                          primary_key = True,
+                          )
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, 
+                         db.ForeignKey('movies.movie_id'),
+                         )
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'),
+                        )
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
 if __name__ == '__main__':
     from server import app
